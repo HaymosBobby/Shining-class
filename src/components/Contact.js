2819ca3bfcdb1bd8a221bf6c4fdeae7f69606384
Akinbande Amos <haymosx@gmail.com>
2024-01-22 16:27:07 +0100

    contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,9 @@
+import { Icon } from "@iconify/react";
+
 const Contact = () => {
   return (
-    <section className="contact">
-      <div className="contact-heading">
+    <section className="contacts">
+      <div className="contacts-heading">
         <h4>Contact</h4>
         <h2>Get in touch</h2>
         <p>
@@ -10,9 +12,50 @@ const Contact = () => {
           achieve success in the mining industry
         </p>
       </div>
-      <div className="contact-container"></div>
+      <div className="contacts-container">
+        <div className="contact">
+          <div className="icon">
+            <Icon icon="bi:envelope" />
+          </div>
+          <h3>Email</h3>
+          <div className="info">
+            <p>
+              suppot@shiningclass.com <br />
+              info@shiningclass.com
+            </p>
+          </div>
+        </div>
+        <div className="contact">
+          <div className="icon">
+            <Icon icon="bx:phone-call" />
+          </div>
+          <h3>Phone</h3>
+          <div className="info">
+            <p>
+              +234 803 3333 333 <br />
+              +234 803 3333 333
+            </p>
+          </div>
+        </div>
+        <div className="contact">
+          <div className="icon">
+            <Icon icon="ion:location-outline" />
+          </div>
+          <h3>Address</h3>
+          <div className="info">
+            <p>
+              happiness street melbourne degung city,
+              <br />
+              Ontario, Nigeria
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
 
 export default Contact;
+
+//
+//
