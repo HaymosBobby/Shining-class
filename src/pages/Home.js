139526909ed17bef58c7c6cf566f2096f84aba83
Akinbande Amos <haymosx@gmail.com>
2024-01-22 09:16:02 +0100

    expert

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,5 @@
 import About from "../components/About";
+import Expert from "../components/Expert";
 import Hero from "../components/Hero";
 import Projects from "../components/Projects";
 import Services from "../components/Services";
@@ -14,6 +15,7 @@ const Home = () => {
       <Services servicesData={servicesData} />
       <Testimonial testimonialsData={testimonialsData} />
       <Projects />
+      <Expert />
     </main>
   );
 };
